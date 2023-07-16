56bc9d45c0227bee330f67c814f7bee5630eddbe
rusrom <19729227+rusrom@users.noreply.github.com>
2023-07-16 12:08:05 +0300

    structures for todo application

---
+++ todo.go
@@ -0,0 +1,26 @@
+package todo
+
+type ListTodo struct {
+	Id          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+
+type UserList struct {
+	Id     int
+	UserId string
+	ListId string
+}
+
+type ItemTodo struct {
+	Id          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Done        bool   `json:"done"`
+}
+
+type ItemList struct {
+	Id     int
+	ListId string
+	ItemId string
+}

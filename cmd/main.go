a85ccf07e614911d8793e3625bb19b94cfdee51a
rusrom <19729227+rusrom@users.noreply.github.com>
2023-07-19 13:49:18 +0300

    add configuration .yml and viper

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,27 @@ import (
 	"github.com/rusrom/yt-todo/pkg/handler"
 	"github.com/rusrom/yt-todo/pkg/repository"
 	"github.com/rusrom/yt-todo/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initialization config %s", err.Error())
+	}
+
 	repos := repository.NewTodoRepository()
 	services := service.NewTodoService(repos)
 	handlers := handler.NewTodoHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error while running web server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }

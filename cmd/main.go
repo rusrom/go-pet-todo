6e37940ba404b531552e8667d0521494d2b90981
rusrom <19729227+rusrom@users.noreply.github.com>
2023-07-27 20:33:28 +0300

    gracefull shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	todo "github.com/rusrom/yt-todo"
@@ -10,6 +11,8 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -40,9 +43,32 @@ func main() {
 	handlers := handler.NewTodoHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
-		logrus.Fatalf("Error while running web server: %s", err.Error())
+
+	// Gracefull Shutdown block start
+	go func() {
+		if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+			logrus.Fatalf("Error while running web server: %s", err.Error())
+		}
+	}()
+
+	logrus.Info("Todo app started")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+
+	<-quit
+
+	logrus.Info("Todo app shutting down...")
+
+	if err := srv.Shutdown(context.Background()); err != nil {
+		logrus.Errorf("error ocurred on server shutting down: %s", err.Error())
 	}
+
+	if err := db.Close(); err != nil {
+		logrus.Errorf("error ocurred on db connection close: %s", err.Error())
+	}
+
+	// Gracefull Shutdown block end
 }
 
 func initConfig() error {
